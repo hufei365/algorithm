86bad67017277f7331f1fc67a1b0fcc347070f77
hufei <hufeicom@qq.com>
2020-10-23 21:58:26 +0800

    combination-sum && combination-sum-ii

--- src/JavaScript/combination-sum-ii.js
+++ src/JavaScript/combination-sum-ii.js
@@ -4,16 +4,26 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
+    let results = [];
+        candidates.sort( (a,b)=>a-b);
+        for( let i = 0; i < candidates.length; i++){
+            if( candidates[i] === candidates[i-1]){
+                continue;
+            }
+            let next = target - candidates[i];
+            if( next > 0 ){
+                let nextResults = combinationSum2( candidates.slice( i+1 ), next);
+                if( nextResults.length ){
+                    results = results.concat( nextResults.map(m=>{
+                        m.unshift( candidates[i] );
+                        return m;
+                    }))
+                }
+            } else if( next === 0 ){
+                results.push([candidates[i]])
+            }
+        } 
+        return results;
+    };
 
-    var result = [];
-    candidates.forEach( (v, i)=>{
-        var rest = combinationSum2( candidates.slice(i+1), target-v);
-        console.log(rest, target-v)
-        rest.forEach( sub=>{
-            result.push( [v, ...sub])
-        })
-    })
-    return result;
-};
-
-console.log( combinationSum2([2,5,2,1,2], 5))
+console.log(combinationSum2([2, 5, 2, 1, 2], 5))
